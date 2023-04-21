4060beac289237199f7c146ca4ecceb85f9addf8
Jose Luis <wildchamo@gmail.com>
2023-04-21 00:00:29 -0500

    cargo la información en un estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+//link con acceso a la api
 const ENDPOINT = "https://jsonplaceholder.typicode.com/photos";
 
 function App() {
+  const [data, setData] = useState([]); //se crea un estado para guardar los datos que traeremos de la API
+
+  //implento un efecto para ejecutar el código que trae la infor de la api cuando se monta la aplicación
   useEffect(() => {
     fetch(ENDPOINT)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((res) => res.json()) //recibo la información y la convierto a json para ser consumida desde la app
+      .then((data) => setData(data)); //guardo la información en el estado
   }, []);
+
   return <></>;
 }
 
