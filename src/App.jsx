5230447c5055059a23be950d65c96751ccfa7bd5
Jose Luis <wildchamo@gmail.com>
2023-04-20 23:57:49 -0500

    llamamos a la api y mostramos el arreglo en consola

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+
+const ENDPOINT = "https://jsonplaceholder.typicode.com/photos";
 
 function App() {
-
-
-  return (
-    <>
-
-    </>
-  )
+  useEffect(() => {
+    fetch(ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+  return <></>;
 }
 
-export default App
+export default App;
